b644e566c9654d31821e290de5892879b66c35ad
yumnaaa <yumnaakhtar@hotmail.com>
2018-04-28 14:53:11 -0700

    pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//create variable in html. rest is in js
+    //crystals 1-4 (1-12
+    var x = Math.floor((Math.random() * 12) + 1);
+    //randomly generated number (19-120)
+    var x = Math.floor((Math.random() * 120) + 19);
+    //players guess
+    //wins
+    //loses
+//add on click for each crystal
+//add that value to players guess
+    //continuously compare users guess to computer generated number
+    //if equal, win increment
+    //if user guess is greater, losses increment
+
+    
